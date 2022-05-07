ed0a61b2db43ae1f2ca5a0a4d9b027eb0f2e8b9a
Erik Young <erik_young@mac.com>
2022-05-07 15:33:57 -0400

    Applied dark theme.

--- mycelium_ui/pages/_app.js
+++ mycelium_ui/pages/_app.js
@@ -1,12 +1,18 @@
 import '../styles/globals.css'
+import { createTheme } from '@nextui-org/react';
 import { NextUIProvider } from '@nextui-org/react';
 
-
 function MyApp({ Component, pageProps }) {
+
+  const darkTheme = createTheme({
+    type: 'dark',
+    theme: {}
+  });
+
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
+      <NextUIProvider theme={darkTheme} >
+        <Component {...pageProps} />
+      </NextUIProvider>
   )
 }
 
