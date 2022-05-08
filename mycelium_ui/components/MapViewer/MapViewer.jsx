9ae7eb64890d7a613511ea38f6838fb6b7bd9ea8
LostnAustin <chadlengefeld@gmail.com>
2022-05-08 05:32:11 -0500

    created Mapbox component, added dependencies to .json

---
+++ mycelium_ui/components/MapViewer/MapViewer.jsx
@@ -0,0 +1,67 @@
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibXVsdGl2ZXJzZW11ZmZpbiIsImEiOiJjam51cjBhcWwwN2RyM3dudngzeXZ0cHB6In0.kma6XOVomvu4FAmhOTzllQ';
+
+export default function App() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-97.742600);
+  const [lat, setLat] = useState(30.530410);
+  const [zoom, setZoom] = useState(17);
+//   const [pitch, setPitch] = useState(40)
+
+// const iotDevice = {
+//     "type": "FeatureCollection",
+//     "features": [
+//       { "type": "Feature", "properties": { "Name": "VA Medical Center -- Leestown Division", "Address": "XXXXX Whispering Oaks" }, "geometry": { "type": "Point", "coordinates": [ -97.7426, 30.53041 ] } }
+//     ]};
+
+    // map.on('load', () => {
+    //     map.addLayer({
+    //       id: 'iotDevice',
+    //       type: 'symbol',
+    //       source: {
+    //         type: 'geojson',
+    //         data: iotDevice
+    //       },
+    //       layout: {
+    //         'icon-image': 'hospital-15',
+    //         'icon-allow-overlap': true
+    //       },
+    //       paint: {}
+    //     });
+    // });
+        
+
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/multiversemuffin/cl2r8oppl003914pb85ph5vx7',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
+
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on('move', () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
+
+  return (
+    <div>
+      <div className="sidebar">
+        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+      </div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  );
+}
+
+
